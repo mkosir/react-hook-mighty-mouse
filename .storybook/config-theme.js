84b585f5270c01420b9150ac75be4af12e6ad97e
mkosir <kosir.mar@gmail.com>
2019-12-09 22:38:42 +0100

    update storybook config

---
+++ .storybook/config-theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'React Mighty Mouse 🐭',
+  brandUrl: 'https://github.com/mkosir/react-hook-mighty-mouse',
+});
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,10 @@
 import { addParameters, addDecorator, configure } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import theme from './config-theme';
 
 addParameters({
   options: {
-    /**
-     * name to display in the top left corner
-     * @type {String}
-     */
-    name: 'React Mighty Mouse 🐭',
+    theme: theme,
     /**
      * display panel that shows addon configurations
      * @type {Boolean}
